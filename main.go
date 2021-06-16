fab79c00fb2ecfa619f1a4f011369c2ea13f8c21
Arnout <Exxon1800@users.noreply.github.com>
2021-06-16 16:28:21 +0200

    fixes

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
 	"github.com/while1malloc0/hotwire-go-example/models"
@@ -44,13 +43,23 @@ func main() {
 	_ = http.ListenAndServe(":8080", nil)
 }
 
-//search function returns the result of the query
+// search function returns the result of the query
 func search(query string, args []interface{}) (dataList []Room) {
 	var room Room
+
 	rows, err := db.Raw(query, args...).Rows()
 	if err != nil {
 		panic(err)
 	}
+
+	defer func() {
+		_ = rows.Close()
+
+		err = rows.Err()
+		if err != nil {
+			log.Fatalf("Fatal error: %v", err)
+		}
+	}()
 
 	columns, err := rows.Columns()
 	if err != nil {
@@ -65,12 +74,14 @@ func search(query string, args []interface{}) (dataList []Room) {
 	for i := range values {
 		scanArgs[i] = &values[i]
 	}
+
 	for rows.Next() {
 		// get RawBytes from data
 		err = rows.Scan(scanArgs...)
 		if err != nil {
 			panic(err)
 		}
+
 		var value string
 
 		for i, col := range values {
@@ -80,10 +91,11 @@ func search(query string, args []interface{}) (dataList []Room) {
 			} else {
 				value = string(col)
 			}
-			//if columns[i] == "id" {
-			//	room.ID = value
-			//}
-			if columns[i] == "name" {
+
+			switch columns[i] {
+			case "name":
+				room.Name = value
+			case "id":
 				room.Name = value
 			}
 		}
@@ -99,84 +111,106 @@ var final int
 
 func paging(w http.ResponseWriter, r *http.Request) {
 	var (
-		paging PaginateDataStruct
-		result []Room
-		count, query  string
-		args   []interface{}
+		paging       PaginateDataStruct
+		result       []Room
+		count, query string
+		args         []interface{}
 	)
 
-	if r.Method == "POST" {
-		r.ParseForm()
-		count = `SELECT count(*) as frequency FROM rooms`
-		start := r.FormValue("start")
-		end := r.FormValue("length")
-		draw := r.FormValue("draw")
-		searchValue := r.FormValue("search[value]")
+	err := r.ParseForm()
+	if err != nil {
+		log.Fatalf("Fatal error: %v", err)
+	}
 
-		if draw == "1" {
-			rows, err := db.Raw(count).Rows()
+	count = `SELECT count(*) as frequency FROM rooms`
+
+	start := r.FormValue("start")
+	end := r.FormValue("length")
+	draw := r.FormValue("draw")
+	searchValue := r.FormValue("search[value]")
+
+	if draw == "1" {
+		rows, err := db.Raw(count).Rows()
+		if err != nil {
+			log.Fatalf("Fatal error: %v", err)
+		}
+
+		defer func() {
+			_ = rows.Close()
+
+			err = rows.Err()
 			if err != nil {
-				fmt.Printf("QueryRow: %v\n", err)
+				log.Fatalf("Fatal error: %v", err)
 			}
-			defer rows.Close()
-			for rows.Next() {
-				err = rows.Scan(&final)
-				if err != nil {
-					fmt.Println(err)
-				}
+		}()
+
+		for rows.Next() {
+			err = rows.Scan(&final)
+			if err != nil {
+				log.Panicf("Fatal error: %v", err)
 			}
 		}
-		log.Infof("Records Filtered: %d", final)
+	}
 
-		if searchValue != "" {
-			query = `SELECT name FROM rooms
+	if searchValue != "" {
+		query = `SELECT name FROM rooms
 						WHERE name LIKE ? 
 						ORDER BY name
 						Limit ? , ?;`
-			p := searchValue + "%"
-			args = []interface{}{p, start, end}
-			aux := []interface{}{p}
-			result = search(query, args)
+		p := searchValue + "%"
+		args = []interface{}{p, start, end}
+		aux := []interface{}{p}
+		result = search(query, args)
 
-			// Here we obtain the number of results
-			rows,err := db.Raw(`SELECT COUNT(*) FROM rooms
+		// Here we obtain the number of results
+		rows, err := db.Raw(`SELECT COUNT(*) FROM rooms
 			WHERE name LIKE ? 
 			ORDER BY name`, aux...).Rows()
-			if err != nil {
-				fmt.Printf("QueryRow: %v\n", err)
-			}
+		if err != nil {
+			log.Fatalf("Fatal error: %v", err)
+		}
 
-			defer rows.Close()
-			for rows.Next() {
-				err = rows.Scan(&final)
-				if err != nil {
-					fmt.Println(err)
-				}
+		defer func() {
+			_ = rows.Close()
+
+			err = rows.Err()
+			if err != nil {
+				log.Fatalf("Fatal error: %v", err)
 			}
-		} else {
-			query = `SELECT name FROM rooms
+		}()
+
+		for rows.Next() {
+			err = rows.Scan(&final)
+			if err != nil {
+				log.Fatalf("Fatal error: %v", err)
+			}
+		}
+	} else {
+		query = `SELECT name FROM rooms
 			ORDER BY name
 			Limit ? , ?;`
-			args = []interface{}{start, end}
-			result = search(query, args)
-		}
-
-		paging.Data = result
-		paging.Draw = draw
-		paging.RecordsFiltered = final
-		e, err := json.Marshal(paging)
-		if err != nil {
-			fmt.Println(err)
-
-			return
-		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
-		w.Write(e)
-	} else {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		args = []interface{}{start, end}
+		result = search(query, args)
 	}
+
+	paging.Data = result
+	paging.Draw = draw
+	paging.RecordsFiltered = final
+	
+	e, err := json.Marshal(paging)
+	if err != nil {
+		log.Fatalf("Fatal error: %v", err)
+
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, err = w.Write(e)
+	if err != nil {
+		return 
+	}
+
 }
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -195,6 +229,9 @@ func setupDB() *gorm.DB {
 	db, err := gorm.Open(sqlite.Open("chat.db"), &gorm.Config{
 		DisableForeignKeyConstraintWhenMigrating: true,
 	})
+	if err != nil {
+		log.Fatalf("Fatal error: %v", err)
+	}
 
 	log.Println("Running migrations")
 
